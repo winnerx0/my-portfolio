0724fc7f3dc127ea008a7e89b71a67e0db0f17b2
winner <eragbonikpeya@gmail.com>
2026-02-24 18:04:55 +0100

    changes: File changes and UI changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,29 +17,36 @@ const page = () => {
         />
         <h1 className="text-xl font-bold">Winner</h1>
         <h2 className="text-[clamp(20px,1.3vw,3rem)]">Software Engineer</h2>
-        <p className="w-full max-w-xl text-gray-400">
-          I'm Winner a software developer experienced in frontend web
-          development and backend development
+        <p className="text-gray-400">
+          I'm Winner a software developer passionate about solving problems and
+          software solutions.
+        </p>
+        <p className="text-gray-400">
+          I primarily work with Java, Typescript and Go. But I am interested in
+          a lot of other technologies.
         </p>
         <Link
           download
-          href={"/Agbonikpeya-Eromoigbe-Winner.pdf"}
-          target="parent"
+          href={"/resume.pdf"}
+          target="_blank"
           className="text-gray-400 underline"
         >
           Resume
         </Link>
         <div className="flex gap-2">
-          <Link href={"https://www.linkedin.com/in/eromoigbe-agbonikpeya/"}>
+          <Link
+            href={"https://www.linkedin.com/in/eromoigbe-agbonikpeya/"}
+            target="_blank"
+          >
             <LiaLinkedinIn />
           </Link>
-          <Link href={"https://x.com/winnerx0"}>
+          <Link href={"https://x.com/winnerx0"} target="_blank">
             <FaXTwitter />
           </Link>
-          <Link href={"https://github.com/winnerx0"}>
+          <Link href={"https://github.com/winnerx0"} target="_blank">
             <FaGithub />
           </Link>
-          <Link href={"mailto:eragbonikpeya@gmail.com"}>
+          <Link href={"mailto:winneragbonikpeya@gmail.com"} target="_blank">
             <MdEmail />
           </Link>
         </div>
@@ -47,61 +54,52 @@ const page = () => {
       </div>
       <div className="flex flex-col gap-4 w-full mt-16">
         <h1 className="text-xl font-bold">Projects</h1>
-        <ul className="text-gray-400">
-          <li>
-            <p>
-              <Link href={"https://notivue.vercel.app/"}>• Notivue</Link> - AI
-              Meeting Analyzer
-            </p>
-          </li>
-          <li>
-            <p>
-              <Link href={"https://simon-therapist.vercel.app/"}>• Simon</Link>{" "}
-              - AI Theriapist
-            </p>
+        <ul className="text-gray-400 divide-y">
+          <li className="py-4">
+            <Link
+              href="https://notivue.vercel.app/"
+              className="underline-offset-4 hover:underline transition-all duration-200"
+            >
+              • Notivue
+            </Link>{" "}
+            - AI Meeting Analyzer
           </li>
 
-          <li>
-            <p>
-              <Link href={"https://lyra-pg.vercel.app/"}>• Lyra</Link> - Real
-              time database visualization tool
-            </p>
+          <li className="py-4">
+            <Link
+              href="https://simon-therapist.vercel.app/"
+              className="underline-offset-4 hover:underline transition-all duration-200"
+            >
+              • Simon
+            </Link>{" "}
+            - AI Therapist
+          </li>
+
+          <li className="py-4">
+            <Link
+              href="https://lyra-pg.vercel.app/"
+              className="underline-offset-4 hover:underline transition-all duration-200"
+            >
+              • Lyra
+            </Link>{" "}
+            - Real time database visualization tool
           </li>
         </ul>
-        <hr />
       </div>
 
       <div className="flex flex-col gap-4 w-full mt-16">
         <h1 className="text-xl font-bold">Work</h1>
         <div>
           <h2 className="text-gray-400">Tredbase June 2025 - Present</h2>
-          <p className="text-gray-400">
-            Buiding and maintaining backend servers efficiently and effectively
-          </p>
         </div>
         <hr />
         <div>
           <h2 className="text-gray-400">Luminevent June 2025 - July 2025</h2>
-          <p className="text-gray-400">Built robust backend services</p>
         </div>
         <hr />
         <div>
           <h2 className="text-gray-400">TekkTopia Sept 2024 - Jan 2025</h2>
-          <p className="text-gray-400">Built web applications for businesses</p>
         </div>
-        <hr />
-      </div>
-
-      <div className="flex flex-col gap-4 w-full mt-16">
-        <h1 className="text-xl font-bold">Interests</h1>
-        <h2 className="text-gray-400">
-          I am into backend using spring boot and node js
-        </h2>
-        {/* <h2 className="text-gray-400">
-          I use Go to build cli tools e.g A file uploading tool, file
-          compression etc.
-        </h2> */}
-        {/* <h2 className="text-gray-400">I like using neovim and the terminal</h2> */}
         <hr />
       </div>
     </section>
